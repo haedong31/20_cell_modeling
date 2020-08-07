70e238a8c6e4d82677913891b5f3b95234c4d704
Haedong Kim - Mac <haedong.kim90@gmail.com>
2020-08-06 21:22:02 -0400

    IKsum decomposition debugging
    1. Calculate SSE / 2. Stopping criterion

--- code/diect_decomp_fit.m
+++ code/diect_decomp_fit.m
@@ -12,12 +12,12 @@ raw_trc = raw_trc(1:500001,:);
 
 raw_ko = raw_trc.KO;
 raw_ko = raw_ko ./ cap_ko;
+raw_ko(raw_ko < 0) = 0;
 raw_time = raw_trc.time;
 
 % chop off early phase
 [~, peak_idx] = max(raw_ko);
 raw_ko_rd = raw_ko(peak_idx:end);
-raw_ko_rd(raw_ko_rd < 0) = 0;
 raw_time_rd = raw_time(peak_idx:end);
 raw_time_rd = raw_time_rd - raw_time_rd(1);
 
@@ -25,15 +25,6 @@ raw_trc_rd = table(raw_time_rd, raw_ko_rd);
 raw_trc_rd.Properties.VariableNames = {'time', 'current'};
 ds_trc = downsample(raw_trc_rd, 400);
 
-% peak and tau (of IKsum for initialization)
-[peak, peak_idx] = max(ds_trc.current);
-[~, tau_idx] = min(abs(peak*exp(-1) - ds_trc.current(peak_idx:end)));
-tau = ds_trc.time(peak_idx+tau_idx);
 
 %% run AGA
-[best_amps, best_taus, best_gens, best_chroms] = direct_decomp(6, 6, [peak, tau], 30, 6, 4);
-
-
-%% 
-
-
+[best_fits, best_iters, best_chroms] = direct_decomp(6, 6, ds_trc, 30, 6, 4);
--- code/direct_decomp.m
+++ code/direct_decomp.m
@@ -1,4 +1,4 @@
-function [best_amps, best_taus, best_gens, best_chroms] = direct_decomp(nv_to, nv_Kslow, y, n0, n1, n2)
+function [best_fits, best_iters, best_chroms] = direct_decomp(nv_to, nv_Kslow, trc, n0, n1, n2)
     global num_var;
     global N0;
     global N1;
@@ -10,13 +10,8 @@ function [best_amps, best_taus, best_gens, best_chroms] = direct_decomp(nv_to, n
     N2 = n2;
     sig_ctl = [];
     best_fits = [];
-    best_amps = [];
-    best_taus = [];
-    best_gens = [];
+    best_iters = [];
     best_chroms = [];
-    
-    amp_tol = 0.5;
-    tau_tol = 0.5;
 
     % initialization
     low_to = [0.0, 0.0, 0.0, 20.0, 2.0, 0.2];
@@ -36,106 +31,87 @@ function [best_amps, best_taus, best_gens, best_chroms] = direct_decomp(nv_to, n
     init_gen_Kslow2 = zeros(N0, nv_Kslow);
     for j=1:nv_Kslow
         unif2 = makedist('Uniform', 'lower',low_Kslow(j), 'upper',high_Kslow(j));
-        unif3 = makedist('Uniform', 'lower',low_Kslow(j), 'upper',high_Kslow(j));
         init_gen_Kslow1(:,j) = random(unif2, N0, 1);
-        init_gen_Kslow2(:,j) = random(unif3, N0, 1);
+        init_gen_Kslow2(:,j) = random(unif2, N0, 1);
     end
     
     % Iss initialization
-    unif4 = makedist('Uniform', 'lower',2.08, 'upper',13.50);
-    init_ss = random(unif4, N0, 1);
+    unif3 = makedist('Uniform', 'lower',2.08, 'upper',13.50);
+    init_ss = random(unif3, N0, 1);
     
     % consolidated initial population
     init_gen = [init_gen_to, init_gen_Kslow1, init_gen_Kslow2, init_ss];
     
     cnt = 1;
-    [fits, amp_dels, tau_dels] = eval_fn(init_gen, y);
+    fits = eval_fn(init_gen, trc);
     [bf, bf_idx] = min(fits);
-    bamp = amp_dels(bf_idx);
-    btau = tau_dels(bf_idx);
     bchrom = init_gen(bf_idx,:);
 
-    fprintf('Initial best fit: %f|Amp: %f|Tau: %f \n', bf, amp_dels(bf_idx), tau_dels(bf_idx));
+    fprintf('Initial best fit: %f \n', bf);
     disp(bchrom)
 
     best_cnt = 1;
     best_fits = [best_fits, bf];
-    best_amps = [best_amps, bamp];
-    best_taus = [best_taus, btau];
-    best_gens = [best_gens, 1];
+    best_iters = [best_iters, 1];
     best_chroms = [best_chroms; bchrom];
     
-    new_gen = evolve(init_gen, amp_dels, tau_dels);
+    new_gen = evolve(init_gen, fits);
     
     while 1
         tic
-        fprintf('\n Generation %i \n', cnt)
         cnt = cnt + 1;
-        [fits, amp_dels, tau_dels] = eval_fn(new_gen, y);
+        fprintf('\n Generation %i \n', cnt)
+        
+        fits = eval_fn(new_gen, trc);
         [bf, bf_idx] = min(fits);
-        bamp = amp_dels(bf_idx);
-        btau = tau_dels(bf_idx);
         bchrom = new_gen(bf_idx,:);
         
-        % stopping tolerance
-        if (bamp <= amp_tol) && (btau <= tau_tol)
-            fprintf('Termination: %f|Amp: %f|Tau: %f \n', bf, bamp, btau);
+        % stopping criterion
+        if (cnt <= 1000) || (bf <= 1)
+            fprintf('Termination: %f \n', bf);
             disp(bchrom)
 
             best_fits = [best_fits, bf];
-            best_amps = [best_amps, bamp];
-            best_taus = [best_taus, btau];
-            best_gens = [best_gens, cnt];
+            best_iters = [best_iters, cnt];
             best_chroms = [best_chroms; bchrom];
             
             break
         end
 
         if (bf < best_fits(best_cnt))
-            fprintf('Best fit is updated: %f|Amp: %f|Tau: %f \n', bf, bamp, btau)
+            fprintf('Best fit is updated: %f \n', bf)
             disp(bchrom)
 
             best_cnt = best_cnt + 1;
             best_fits = [best_fits, bf];
-            best_amps = [best_amps, bamp];
-            best_taus = [best_taus, btau];
-            best_gens = [best_gens, cnt];
+            best_iters = [best_iters, cnt];
             best_chroms = [best_chroms; bchrom];
-            % damp_factor = (bf-min_tol)/(max_tol-min_tol)*0.5 + 0.5;
-            % fprintf('Damping factor %f \n', damp_factor)
         end 
         
-        new_gen = evolve(new_gen, amp_dels, tau_dels);
+        new_gen = evolve(new_gen, fits);
         toc
     end 
 end
 
-function new_gen = evolve(chrom, amp_dels, tau_dels)
+
+function new_gen = evolve(chrom, fits)
     global num_var;
     global N0;
     global N1;
     global N2;
     global sig_ctl;
 
-    fits = amp_dels + tau_dels;
-    [~, bf_idx] = min(fits);
-    bamp = amp_dels(bf_idx);
-    btau = tau_dels(bf_idx);
-    
-    [bfits, super_idx] = mink(fits, N1);
+    [~, super_idx] = mink(fits, N1);
     elites = chrom(super_idx, :);
-    sigs = std(elites);
-
+    
     mean_elite = mean(elites, 1);
     elites(end,:) = mean_elite;
-
-%     disp([bamp, btau])
-%     disp(bfits)
-%     disp(sigs)
-%     disp(elites)
-
+    
+    sigs = std(elites);
     sig_ctl = [sig_ctl; sigs];
     pooled_sigs = mean(sig_ctl,1);
+
+%     disp(elites)
 %     disp(pooled_sigs)
 
     % breeding
@@ -155,7 +131,8 @@ function new_gen = evolve(chrom, amp_dels, tau_dels)
     end
 end
 
-function [fits, amp_dels, tau_dels] = eval_fn(chrom, y)
+
+function fits = eval_fn(chrom, trc)
     global num_var;
     global N0;
 
@@ -166,9 +143,6 @@ function [fits, amp_dels, tau_dels] = eval_fn(chrom, y)
     Ek = -91.1;
     
     fits = zeros(1, N0);
-    amp_dels = zeros(1, N0);
-    tau_dels = zeros(1, N0);
-    wrn_idx = [];
     for i=1:N0
         try
             [t, ~, A] = IKsum(chrom(i,1:num_var), holding_p, holding_t, P1, P1_t, Ek);
@@ -177,36 +151,31 @@ function [fits, amp_dels, tau_dels] = eval_fn(chrom, y)
             IKslow2 = A(:,15);
             Iss = chrom(i,end);
             
-            IKsum_trc = Ito + IKslow1 + IKslow2 + Iss;
+            IKsum_trc = Ito + IKslow1 + IKslow2;
             
             wrong_shape_iden = any(IKsum_trc < 0);
-            [peak, peak_idx] = max(IKsum_trc);
+            [~, peak_idx] = max(IKsum_trc);
             if (wrong_shape_iden == 1) || (t(peak_idx) < holding_t)
                 % fprintf('Wrong shape at %i \n', i);
                 % disp(chrom(i,:));
-                    
-                wrn_idx = [wrn_idx, i];
-                amp_dels(i) = 15000;
-                tau_dels(i) = 15000;
-                fits(i) = amp_dels(i) + tau_dels(i);
+                fits(i) = 100000000;
             else
-                amp_dels(i) = abs(peak - y(1));
+                % chop off the early phase
+                IKsum_trc_rd = (IKsum_trc(peak_idx:end) + Iss);
+                t_rd = t(peak_idx:end);
+                t_rd = t_rd - t_rd(1);
 
-                [~, tau_idx] = min(abs(peak*exp(-1) - IKsum_trc(peak_idx:end)));
-                tau_dels(i) = abs(t(tau_idx)-y(2));
-                
-                fits(i) = amp_dels(i) + tau_dels(i);
+                appx_time_idx = dsearchn(t_rd, trc.time);
+                ds_IKsum = IKsum_trc_rd(appx_time_idx);
+
+                fits(i) = sum((ds_IKsum - trc.current).^2);
             end
         catch
             % lastwarn
             % lasterr
             % fprintf('Error or warning at %i \n', i);
             % disp(chrom(i,:));
-
-            wrn_idx = [wrn_idx, i];
-            amp_dels(i) = 15000;
-            tau_dels(i) = 15000;
-            fits(i) = amp_dels(i) + tau_dels(i);
+            fits(i) = 100000000;
         end
     end
 end
